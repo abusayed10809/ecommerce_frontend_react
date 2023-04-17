840cccef56d9691c2ea7bedacf8441ed5ce9214e
unknown <abusayed10809@gmail.com>
2023-04-17 19:10:30 +0600

    product list wip

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,23 @@ import Cart from "./pages/Cart";
 import Pay from "./components/Pay";
 import Success from "./components/Success";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 function App() {
+  const user = true;
   return (
     <>
       <Router>
         <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products/:category" element={<ProductList />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route
+            path="/login"
+            element={user ? <Navigate to="/" /> : <Login />}
+          />
+          <Route path="/register" element={<Register />} />
           <Route path="/pay" element={<Pay />} />
           <Route path="/success" element={<Success />} />
         </Routes>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { popularProducts } from "../data";
 import Product from "./Product";
+import axios from "axios";
 
 const Container = styled.div`
   padding: 20px;
@@ -10,12 +11,77 @@ const Container = styled.div`
   justify-content: space-between;
 `;
 
-const Products = () => {
+const Products = ({ cat, filters, sort }) => {
+  console.log(cat);
+  console.log(filters);
+  console.log(sort);
+
+  // cat && (filters = {category:cat});
+  // console.log(filters);
+
+  // sort && (filters = {sort:sort});
+  // console.log(filters);
+
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  /// getting all products from api
+  /// setting products
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(
+          cat
+            ? `http://localhost:5000/api/products?category=${cat}`
+            : "http://localhost:5000/api/products"
+        );
+        console.log("inside get products");
+        console.log(res);
+        setProducts(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getProducts();
+  }, [cat]);
+
+  ///  filtering by size and color
+  useEffect(() => {
+    if (filters != null) {
+      setFilteredProducts(
+        products.filter((item) =>
+          Object.entries(filters).every(([key, value]) =>
+            item[key].includes(value)
+          )
+        )
+      );
+    }
+  }, [products, filters]);
+
+  /// filtering by sort
+  useEffect(() => {
+    if (sort === "newest") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.createdAt - b.createdAt)
+      );
+    } else if (sort === "asc") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.price - b.price)
+      );
+    } else {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => b.price - a.price)
+      );
+    }
+  }, [sort]);
+
   return (
     <Container>
-      {popularProducts.map((item) => (
-        <Product item={item} key={item.id} />
-      ))}
+      {cat
+        ? filteredProducts.map((item) => <Product item={item} key={item._id} />)
+        : products
+            .slice(0, 8)
+            .map((item) => <Product item={item} key={item._id} />)}
     </Container>
   );
 };
